f062478f6a13ab18a5c9b30389cfb71ffa906b8d
smullinmatteo <smullinmatteo@gmail.com>
2021-05-28 10:04:38 -0700

    builtout soundcloud js with dynamic cards generated by track search. Each card is a dynamic div creation with image, header, box outline, and bbutton. Next steps are making info dynamically sourced.

--- JS Coursework/SoundCloudPlayer/javascript/main.js
+++ JS Coursework/SoundCloudPlayer/javascript/main.js
@@ -3,7 +3,7 @@
 
 // STEP 2. QUERY SOUNDCLOUD API
 
-var SoundCloudAPI = {};
+SoundCloudAPI = {};
 
 SoundCloudAPI.initialize = function() {
 
@@ -22,13 +22,70 @@ SoundCloudAPI.getTrack = function(inputValue) {
 	  q: inputValue
 	}).then(function(tracks) {
 	  console.log(tracks);
+	  SoundCloudAPI.renderTracks(tracks);
 	});
 
 }
 
 SoundCloudAPI.getTrack("Rilo Kiley");
 
+SoundCloudAPI.renderTracks = function(tracks) {
+
+	tracks.forEach(function(track){
+
+		//create card
+		card = document.createElement('div');
+		card.classList.add('card');
+
+		//create image
+		imageDiv = document.createElement('div');
+		imageDiv.classList.add('image');
+
+		image_img = document.createElement('img');
+		image_img.classList.add('image_img');
+		image_img.src = track.artwork_url || 'https://edm.com/.image/t_share/MTU5NDY5Nzk2NTUzOTI1OTA1/soundcloud.png';
+		imageDiv.appendChild(image_img);
+
+		//content
+		content = document.createElement('div');
+		content.classList.add('content');
+
+		header = document.createElement('div');
+		header.classList.add('header');
+		header.innerHTML = '<a href ="#" target="_blank">science vs. romance</a>';
+
+		//button
+		button =document.createElement('div');
+		button.classList.add('ui', 'bottom', 'attached', 'button', 'js-button');
+
+		icon = document.createElement('div');
+		icon.classList.add('add', 'icon');
+
+		buttonText = document.createElement('span');
+		buttonText.innerHTML = 'Add to Playlist';
+
 // STEP 3. DISPLAY RESULTS AS CARDS
+
+		//appendall
+		content.appendChild(header);
+		button.appendChild(buttonText);
+
+		card.appendChild(imageDiv);
+		card.appendChild(content);
+		card.appendChild(button);
+
+		searchResults = document.querySelector('.js-search-results');
+		searchResults.appendChild(card);
+
+
+	});
+
+
+}
+
+
+
+
 
 // STEP 4. ADD SELECTED TO PLAYLIST AND AUTO PLAY
 
